4e2c0df8ebec7575dc5b75a9d8d06321f22ba430
ChrisT <christau90s@gmail.com>
2022-01-04 19:38:20 +0200

    vue template interpolation

--- lua/user/lsp/settings/vuels.lua
+++ lua/user/lsp/settings/vuels.lua
@@ -5,7 +5,10 @@ return {
                     completion = {
                         autoImport = true
                     },
-                    useWorkspaceDependencies = true
+                    useWorkspaceDependencies = true,
+                    experimental = {
+                        templateInterpolationService = true,
+                    }
                 }
             }
         }
