96bc3ca22e663b17e3d669a7f0a3049ded0c296f
ChrisT <christau90s@gmail.com>
2022-03-13 16:41:22 +0200

    -

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,4 +3,5 @@ vim.cmd('syntax on')
 vim.cmd(':colorscheme nvcode')
 vim.cmd(':hi Normal guibg=NONE ctermbg=NONE') -- darker background
 vim.cmd(':hi LineNr guibg=NONE ctermbg=NONE')
+vim.cmd(':hi SignColumn guibg=NONE') -- sign column background
 
