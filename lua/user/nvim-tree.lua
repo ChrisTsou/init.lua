800034b1b01c17ff0ae085835afd0a629df97265
Christos Tsoulias <christau90s@gmail.com>
2022-03-21 10:32:29 +0200

    fix nvim tree bind

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -98,7 +98,7 @@ nvim_tree.setup {
                 { key = "<S-CR>",                       cb = tree_cb("close_node") },
                 { key = "S",                            cb = tree_cb("first_sibling") },
                 { key = "T",                            cb = tree_cb("last_sibling") },
-                { key = "I",                            cb = tree_cb("toggle_ignored") },
+                { key = "I",                            cb = tree_cb("toggle_git_ignored") },
                 { key = "H",                            cb = tree_cb("toggle_dotfiles") },
                 { key = "R",                            cb = tree_cb("refresh") },
                 { key = "a",                            cb = tree_cb("create") },
