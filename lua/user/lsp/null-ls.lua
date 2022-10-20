c31dab5e33b29b72df38097e2bb735833b5a3130
ChrisTsou <ChrisTau90s@gmail.com>
2022-10-20 22:14:12 +0300

    feat: add dictionary on null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -7,6 +7,7 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local hover = null_ls.builtins.hover
 
 null_ls.setup({
 	debug = true,
@@ -16,5 +17,7 @@ null_ls.setup({
 
 		formatting.prettier,
 		formatting.stylua,
+
+        hover.dictionary,
 	},
 })
