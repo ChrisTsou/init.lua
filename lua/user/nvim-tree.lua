136c4268041e78413c078e71af8355862db79c0f
ChrisTsou <ChrisTau90s@gmail.com>
2023-02-14 21:34:53 +0200

    try fix nvim tree but fail

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -13,11 +13,6 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-    },
     sync_root_with_cwd = true,
     diagnostics = {
         enable = true,
@@ -67,3 +62,35 @@ nvim_tree.setup({
         },
     },
 })
+
+local function open_nvim_tree(data)
+  local IGNORED_FT = {
+        "alpha",
+        "startify",
+        "dashboard",
+  }
+
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- &ft
+  local filetype = vim.bo[data.buf].ft
+
+  -- only files please
+  if not real_file and not no_name then
+    return
+  end
+
+  -- skip ignored filetypes
+  if vim.tbl_contains(IGNORED_FT, filetype) then
+    return
+  end
+
+  -- open the tree but don't focus it
+  require("nvim-tree.api").tree.open({ focus = false })
+end
+
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
