c3c2ae464c8589356a3ac5d97f51f50c8913a764
ChrisTsou <ChrisTau90s@gmail.com>
2023-02-28 00:43:09 +0200

    mfeat: luasnip enhance

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -5,9 +5,11 @@ end
 
 luasnip.setup({
     enable_autosnippets = true,
+    update_events = { "TextChanged", "TextChangedI" },
+    store_selection_keys = "<Tab>",
 })
 
-vim.cmd[[
+vim.cmd([[
 " Use Tab to expand and jump through snippets
 imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
 smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
@@ -15,7 +17,7 @@ smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Ta
 " Use Shift-Tab to jump backwards through snippets
 imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-]]
+]])
 
 require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/lua/snippets/"})
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets/" })
