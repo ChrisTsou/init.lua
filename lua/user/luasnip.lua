55f5a31901420704b599c21109c4579af9bb1c82
ChrisTsou <ChrisTau90s@gmail.com>
2023-02-28 21:18:54 +0200

    fix: luasnip jump points

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -7,16 +7,19 @@ luasnip.setup({
     enable_autosnippets = true,
     update_events = { "TextChanged", "TextChangedI" },
     store_selection_keys = "<Tab>",
+    -- clear jump points
+    region_check_events = "InsertEnter",
+	delete_check_events = "TextChanged,InsertLeave",
 })
 
 vim.cmd([[
 " Use Tab to expand and jump through snippets
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+imap <silent><expr> <Tab> luasnip#expand_or_locally_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+smap <silent><expr> <Tab> luasnip#locally_jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
 
 " Use Shift-Tab to jump backwards through snippets
-imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+imap <silent><expr> <S-Tab> luasnip#locally_jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#locally_jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]])
 
 require("luasnip.loaders.from_vscode").lazy_load()
