e7e0ecfb861fb9e1956067301f32ee9a67fb40bd
Christos Tsoulias <chris@BRE1-AAD-862651.>
2023-09-22 12:50:58 +0200

    fix: remove config

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -11,17 +11,6 @@ npairs.setup({
         javascript = { "string", "template_string" },
         java = false,
     },
-    fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0, -- Offset from pattern match
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-    },
 })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
@@ -29,4 +18,4 @@ local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
     return
 end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({}))
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
