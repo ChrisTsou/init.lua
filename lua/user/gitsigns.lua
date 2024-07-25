cd49ead85319f147037bb2c427d2d05873b11b28
Christos Tsoulias <chris@BRE-W-97TDSG3.>
2024-07-25 13:32:54 +0200

    fix gitsigns

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -4,12 +4,20 @@ if not status_ok then
 end
 
 gitsigns.setup({
+    -- signs = {
+    --     add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    --     change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    --     delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    --     topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    --     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    -- },
     signs = {
-        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "󰍵" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "│" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -1,3 +1,4 @@
+-- TODO Update keybindings for whichkey v3
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
     return
